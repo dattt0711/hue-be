8299542689497617660113ae5ef036af49fc1b68
dat <phidat0711@gmail.com>
2023-05-16 19:45:59 +0700

    init

---
+++ src/models/Users.js
@@ -0,0 +1,40 @@
+const mongoose = require('mongoose');
+
+const Schema = mongoose.Schema;
+const { ROLE } = require('../utils/constants');
+const { common } = require('./Common');
+<<<<<<< HEAD
+const ObjectId = mongoose.Types.ObjectId;
+
+=======
+>>>>>>> 490bf8a227a7e82d1ddc39f750872a6157ade5b9
+const usersBase = {
+    username: {
+        type: String, trim: true,
+    },
+    fullName: {
+        type: String, trim: true,
+    },
+    dob: {
+        type: String, trim: true,
+    },
+    phone: {
+        type: Number, trim: true,
+    },
+    password: {
+        type: String, trim: true
+    },
+    email: {
+        type: String, trim: true
+    },
+    wishList: [{
+        type: ObjectId, trim: true, ref: 'products'
+    }],
+    isAdmin: {
+        type: Boolean, default: false
+    }
+}
+const users = { ...usersBase, ...common };
+const UsersSchema = new Schema(users, { versionKey: false });
+const UsersModels = mongoose.model('users', UsersSchema);
+module.exports = UsersModels;
